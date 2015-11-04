bc105638f41e0548e3b8fc8164ffe04917fb3f78
mdkdrnevich <mdkdrnevich@gmail.com>
2015-11-04 13:59:04 -0500

    Changed PHYSICS in physics.py so that it has a managing function which takes a data file and returns the training, validation, and testing sets at once. There also was no need for most of the variables used in the PHYSICS class so they were removed.
    Also changed layers4_width300_lr005_m200_do05top_raw.py so that it implements the new physics.py properly.

--- csvData.py
+++ csvData.py
@@ -5,31 +5,32 @@ from math import floor
 
 def split(string, sep):
     temp = re.split("[{0}]+".format(sep), string) # splits the string around sep, i.e. isolates instances of sep
-    temp.pop()
+    temp.pop() # Remove the empty string at the end
     return temp
 
 def readFile(path, sep):
     data = np.array([split(line, sep) for line in file(path)], dtype=np.float32) # reads the data into a numpy array efficiently and converts the strings to 32-bit floats
     return data, data.shape[0], data.shape[1] # returns array, rows, columns
 
-def getData(pathTrain, pathTest):
-    testD, testROWS, testCOLS = readFile(pathTest, ',')
-    trainD, trainROWS, trainCOLS = readFile(pathTrain, ',')
+def getData(pathData, trainPercent, validPercent):
+    trainD, trainROWS, trainCOLS = readFile(pathData, ',')
 
     # Print some examples
-    print("test rows {0}; cols {1}".format(testROWS, testCOLS))
     print("train rows {0}; cols {1}".format(trainROWS, trainCOLS))
     for i in xrange(10):
         print ('Label: {0} \t Train: {1}'.format(trainD[i, 0], trainD[i, 1:]))
 
-    trainPercent = 0.75
-    cutoff = floor(trainPercent*trainROWS)
+    trCutoff = floor(trainPercent*trainROWS)
+    vaCutoff = trCutoff + floor(validPercent*trainROWS)
+
+    # Test data will be the remainder
 
     # Should we shuffle a copy or the actual data?
 
     np.random.shuffle(trainD)
 
-    trainData = {'data': trainD[:cutoff, 1:], 'labels': trainD[:cutoff, 0], 'size': lambda: (cutoff, trainD.shape[1])}
-    valData = {'data': trainD[cutoff:, 1:], 'labels': trainD[cutoff:, 0], 'size': lambda: (trainROWS-cutoff, trainD.shape[1])}
+    trainData = {'data': trainD[:trCutoff, 1:], 'labels': trainD[:trCutoff, 0], 'size': lambda: (trCutoff, trainD.shape[1])}
+    valData = {'data': trainD[trCutoff:vaCutoff, 1:], 'labels': trainD[trCutoff:vaCutoff, 0], 'size': lambda: (vaCutoff-trCutoff, trainD.shape[1])}
+    testData = {'data': trainD[trCutoff+vaCutoff:, 1:], 'labels': trainD[trCutoff+vaCutoff:, 0], 'size': lambda: (trainROWS-vaCutoff, trainD.shape[1])}
 
-    return trainData, valData, testD
+    return trainData, valData, testData
--- physics.py
+++ physics.py
@@ -2,133 +2,46 @@
 #__authors__ = "Peter Sadowski"
 # May 2014
 
+###########
+## CHANGES
+##  Changed PHYSICS so that it has a managing function which takes a data file
+##  and returns the training, validation, and testing sets at once. There also
+##  was no need for most of the variables used in the PHYSICS class so they were
+##  removed.
+###########
+
 from pylearn2.datasets import dense_design_matrix
 from pylearn2.datasets import control
 from pylearn2.utils import serial
 import os
 import numpy as np
 import pickle as pkl
-import csv
+import csvData
+from os import sep
 
-class PHYSICS(dense_design_matrix.DenseDesignMatrix):
-    def __init__(self, 
+def PHYSICS(dataPath,
+            trainPerc,
+            validPerc):
+    benchmark = dataPath.split(sep)[-1].split('.')[0] # Returns the name of the file without its extension
+    train, valid, test = csvData.getData(dataPath, trainPerc, validPerc)
+    return (_PHYSICS(train, 'train', benchmark),
+            _PHYSICS(valid, 'valid', benchmark),
+            _PHYSICS(test, 'test', benchmark))
+
+class _PHYSICS(dense_design_matrix.DenseDesignMatrix):
+    def __init__(self,
+                 data,
                  which_set,
-                 benchmark,
-                 derived_feat=True,
-                 version='',
-                 seed=None, # Randomize data order if seed is not None
-                 start=0, 
-                 stop=np.inf):
+                 benchmark):
 
         self.args = locals()
-        path = os.environ['PYLEARN2_DATA_PATH']
-
-        if derived_feat == 'False':
-            derived_feat = False
-        elif derived_feat == 'True':
-            derived_feat = True
-
-        if benchmark == 1:
-            inputfile = '%s/HIGGS.csv' % path
-        elif benchmark == 2:
-            inputfile = '%s/SUSY.csv' % path
         
         # Need to allocate two arrays X (inputs) and y (targets)
-        # We know the size so let's allocate them from the outset instead
-
-        # Define set of training, testing, and validation sets
-        if benchmark == 1:
-            # HIGGS
-            ntrain = 10000000 
-            nvalid = 500000 
-            ntest  = 500000
-        elif benchmark == 2:
-            # SUSY
-            ntrain = 4000000 
-            nvalid = 500000 
-            ntest  = 500000
-
-
-        if which_set == 'train':
-            offset = 0
-            nrows = ntrain
-        elif which_set == 'valid':
-            offset = ntrain
-            nrows = nvalid
-        elif which_set == 'test':
-            offset = ntrain+nvalid
-            nrows = ntest
-
-        # Define the feature lists and relevant columns
-        if benchmark == 1:
-            if derived_feat == 'only':
-                xcolmin = 22
-                xcolmax = 29
-                ycolmin = 0
-                ycolmax = 1
-            elif not derived_feat:
-                xcolmin = 1
-                xcolmax = 22
-                ycolmin = 0
-                ycolmax = 1
-            else:
-                xcolmin = 1
-                xcolmax = 29
-                ycolmin = 0
-                ycolmax = 1
-        elif benchmark == 2:
-            if derived_feat == 'only':
-                xcolmin = 9
-                xcolmax = 19
-                ycolmin = 0
-                ycolmax = 1
-            if not derived_feat:
-                xcolmin = 1
-                xcolmax = 9
-                ycolmin = 0
-                ycolmax = 1
-            else:
-                xcolmin = 1
-                xcolmax = 19
-                ycolmin = 0
-                ycolmax = 1
-
-
-        # Limit number of samples
-        stop = min(stop,nrows)
-        X = np.empty([stop,xcolmax-xcolmin], dtype='float32')
-        y = np.empty([stop,ycolmax-ycolmin], dtype='float32')
-
-        # Randomize data order.
-        indices = np.arange(X.shape[0])
-        if seed:
-            rng = np.random.RandomState(42)  # reproducible results with a fixed seed
-            rng.shuffle(indices)
-
-        # Finally, let's read in the data
-        reader = csv.reader(open(inputfile))
-        nskipped = 0
-        nread = 0
-        for row in reader:
-
-            # Possibly skip some events at the start
-            if nskipped < offset:
-                nskipped += 1
-                continue
-
-            # Stop when we get to the number we want
-            if nread >= stop:
-                break
-
-            # If we're here, it means we want to save this result
-            X[indices[nread]] = row[xcolmin:xcolmax]
-            y[indices[nread]] = row[ycolmin:ycolmax]
-            nread += 1
 
         print 'Data loaded: benchmark {} ({})'.format(benchmark,which_set)
 
         # Initialize the superclass. DenseDesignMatrix
-        super(PHYSICS,self).__init__(X=X, y=y)
+        super(PHYSICS,self).__init__(X=data['data'], y=data['labels'])
         
     def standardize(self, X):
         """
