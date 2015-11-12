b591476fc5a317e6671d306121bfbed05881eb8f
mdkdrnevich <mdkdrnevich@gmail.com>
2015-11-12 17:45:19 -0500

    Made a few (theoretical) speed improvements on my accuracy algorithm for gpu usage.

--- osu/accuracyTest.py
+++ osu/accuracyTest.py
@@ -12,7 +12,7 @@
 from __future__ import division, print_function
 from pylearn2.train_extensions import TrainExtension
 import theano.tensor as T
-from theano import function
+from theano import function, shared
 import os
 
 # An instance of this should be provided in the Train class as extensions=[<instance>]
@@ -23,10 +23,15 @@ class AccuracyMonitor(TrainExtension):
         print("Writing to {}".format(os.path.splitext(save_path)[0]+"_classification_accuracy.csv"))
         self.save_file = open(os.path.splitext(save_path)[0]+"_classification_accuracy.csv", 'w')
 
-        X = T.fmatrix('X')
-        self.y_hat = function([X], model.fprop(X))
-        self.accuracy = lambda x_1, x_2: ((((x_1 - train.y) > -0.5)==((x_1 - train.y) < 0)).sum(),
-                                          (((x_2 - test.y) > -0.5)==((x_2 - test.y) < 0)).sum())
+        X_1 = T.fmatrix('X1')
+        X_2 = T.fmatrix('X2')
+        X_3 = T.fmatrix('X3')
+        trainY = shared(train.y)
+        validY = shared(valid.y)
+        testY = shared(test.y)
+        self.y_diffs = function([X_1, X_2, X_3], [model.fprop(X_1)-trainY, model.fprop(X_2)-validY, model.fprop(X_3)-testY])
+
+        self.accuracy = lambda x1, x2, x3: (((diff > -0.5)==(diff < 0)).sum() for diff in self.y_diffs(x1, x2, x3))
 
         self.train_data_size = train.y.shape[0]
         self.test_data_size = test.y.shape[0]
@@ -34,7 +39,7 @@ class AccuracyMonitor(TrainExtension):
 
     # This is called before the first epoch and at the end of each epoch
     def on_monitor(self, model, dataset, algorithm):
-        train_accuracy, test_accuracy = self.accuracy(self.y_hat(self.args['train'].X), self.y_hat(self.args['test'].X))
+        train_accuracy, valid_accuracy, test_accuracy = self.accuracy(self.args['train'].X, self.args['valid'].X, self.args['test'].X)
 
         train_percent = train_accuracy/self.train_data_size
         test_percent = test_accuracy/self.test_data_size
