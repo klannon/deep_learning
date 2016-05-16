3726cef7eb6425705755a7fdd07599f3e3534027
Colin Dablain <colin.dablain@gmail.com>
2016-05-16 13:27:00 -0400

    Added visualization scripts under /dashboard

---
+++ dashboard/utils.py
@@ -0,0 +1,10 @@
+import os
+
+def get_datasets():
+    return os.listdir("data")
+
+def get_files_from_dataset(dataset):
+    return os.listdir("data/%s" % dataset)
+    
+if __name__ == "__main__":
+    print get_datasets()
