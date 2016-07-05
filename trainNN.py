a96855ce8bcac590999dded549084fa1bb5eea1e
mdkdrnevich <mdkdrnevich@gmail.com>
2016-07-05 11:45:08 -0400

    Uploading changes for testing on the CRC.

--- trainNN.py
+++ trainNN.py
@@ -1,9 +1,18 @@
-from __future__ import print_function
+from __future__ import print_function, division
 
 import datetime, os, sys, time, argparse
-
-from keras.layers import Dense, Dropout, Input, Merge, merge
-from keras.models import Sequential, model_from_json
+# Need Keras >= 1.0.5
+import theano.tensor as T
+from theano.tensor.shared_randomstreams import RandomStreams
+import theano
+from theano import function, printing
+import json
+#theano.config.exception_verbosity="high";
+theano.config.traceback.limit = 20
+import keras.backend as K
+from keras.layers import Dense, Dropout, Input, Merge, merge, Lambda
+from keras.engine.topology import Layer
+from keras.models import Sequential, model_from_json, Model
 from keras.optimizers import Adam
 from keras.regularizers import l1, l2
 
@@ -38,6 +47,90 @@ def load_model(exp_name):
     model.set_weights(np.load(exp_dir+"weights.npy"))
     return model
 
+def E(indices, step=4):
+    dim = len(indices)*step
+    I = np.zeros((dim,dim))
+    for row, col in zip(xrange(len(indices)), indices):
+        mask = I[step*row:step*(row+1), step*col:step*(col+1)]
+        np.fill_diagonal(mask, 1)
+    return I
+
+def gen_permutations(num_b_jets, num_jets, num_leptons):
+    _leps = set(range(num_leptons))
+    _bjets = set(range(num_b_jets))
+    _jets = list(range(num_jets))
+    for l_1 in _leps:
+        l_2 = tuple(_leps - {l_1,})[0]
+        for b_1 in _bjets:
+            b_2 = tuple(_bjets - {b_1,})[0]
+            for j1_1 in _jets:
+                for j1_2 in _jets[_jets.index(j1_1)+1:]:
+                    remains_5 = list(set(_jets) - {j1_1, j1_2})
+                    for j2_1 in remains_5:
+                        for j2_2 in remains_5[remains_5.index(j2_1)+1:]:
+                            remains_3 = sorted(list(set(remains_5) - {j2_1, j2_2}))
+                            yield [b_1,
+                                   b_2,
+                                   j1_1+2,
+                                   j1_2+2,
+                                   j2_1+2,
+                                   j2_2+2]+map(lambda j: j+2, remains_3)+[l_1+9,
+                                   l_2+9]
+
+
+class Permute(Layer):
+    def __init__(self, output_dim, permutations, batch_size, **kwargs):
+        self.output_dim = output_dim
+        self.permutations = permutations
+        self.num_p = len(permutations)
+        self.batch_size = batch_size
+        #self.labels = T.zeros((1, self.num_p))
+
+        def _get_labels(x, labels, num_p):
+            skip = x.shape[0] // num_p
+            srng = RandomStreams()
+            self.indices = srng.permutation(n=num_p, size=(1,))[0]
+            T.set_subtensor(labels[:], T.zeros((1, num_p)))
+            T.set_subtensor(labels[0, 0], 1)
+            T.set_subtensor(labels[0, :], labels[0, self.indices])
+            identity = T.zeros((self.num_p, self.num_p * skip))
+            theano.scan(lambda i, identity, skip: T.set_subtensor(identity[i, i * skip:(i + 1) * skip], [1] * skip),
+                        n_steps=self.num_p,
+                        sequences=[T.arange(self.num_p)],
+                        non_sequences=[identity, skip])
+            return T.dot(labels, identity)
+        x = T.dmatrix()
+        labels = T.imatrix()
+        self.get_labels = function([self, x, labels], _get_labels)
+
+        super(Permute, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.transforms = [E(p) for p in self.permutations]
+        self.T = K.variable(self.transforms)
+        self.trainable = False
+
+    def call(self, x, mask=None):
+        skip = x.shape[0] // self.num_p
+
+        rval = K.concatenate([K.dot(x, self.T[i].transpose()) for i in xrange(self.num_p)], axis=0)
+
+        #indexes = printing.Print("Indexes")(indexes)
+        temp = rval.copy()
+        theano.scan(lambda b, x, skip: T.set_subtensor(rval[b::skip], temp[b::skip][self.indices,]),
+                             n_steps=skip,
+                             sequences=[T.arange(skip)],
+                             non_sequences=[x, skip])
+        #return T.flatten(out, outdim=2)
+
+        return rval
+
+    def get_output_shape_for(self, input_shape):
+        return (self.num_p, self.output_dim)
+
+    def compute_mask(self, input, input_mask=None):
+        return None
+
 
 def build(config=None):
 
@@ -62,7 +155,9 @@ def build(config=None):
                                                        os.getpid())).split('.')[0],
                                          'x'.join(map(lambda x: hex(int(x))[2:], str(time.time()).split('.'))))
 
-    ##
+
+    ### OLD MODEL
+    """"##
     # Construct the network
     ##
 
@@ -88,7 +183,61 @@ def build(config=None):
     layer.type = 1
     layer.input_dimension = config["nodes"]
     layer.output_dimension = 2
-    model.add(Dense(output_dim=2, activation="softmax"))
+    model.add(Dense(output_dim=2, activation="softmax"))"""
+
+
+    ### SUPER NET
+
+    perms = list(gen_permutations(2,7,2))
+    """
+    def clean_outputs(x):
+        #x = printing.Print("Batch ")(x)
+        skip = x.shape[0] // len(perms)
+        out, _ = theano.scan(lambda b, x, skip: K.reshape(x[b::skip], (1, x.shape[1] * len(perms))),
+                             n_steps=skip,
+                             sequences=[T.arange(skip)],
+                             non_sequences=[x, skip])
+        return T.flatten(out, outdim=2)"""
+
+    inputs = Input(shape=(44,), name="Event Input")
+
+    sorted_model = Sequential(name="Sorted Model")
+    for ix, layer in enumerate(load_model("ttHLep/S_1to1_small").layers[:-1]):
+        layer.trainable = False
+        sorted_model.add(layer)
+        sorted_model.layers[ix].set_weights(layer.get_weights())
+    #sorted_model(Permute(44, perms, exp.batch_size, name="Permutator")(inputs))
+
+    ### SORTING NET
+    inputs = [Input(shape=(44,), name="Event Input {}".format(i)) for i in xrange(len(perms))]
+    inputs = merge(len(perms)*[inputs])
+    o = sorted_model(merge(inputs))
+    """
+    def clean_sorting_outputs(x):
+        skip = x.shape[0] // len(perms)
+        out, _ = theano.scan(lambda b, x, skip: K.reshape(x[b*skip:(b+1)*skip], (1, x.shape[1] * len(perms))),
+                             n_steps=skip,
+                             sequences=[T.arange(skip)],
+                             non_sequences=[x, skip])
+        return T.flatten(out, outdim=2)
+
+    o = Lambda(clean_sorting_outputs,
+               output_shape=lambda s: (exp.batch_size, 20 * len(perms)),
+               name="Filter")(o)"""
+
+    o = Dense(len(perms), activation="softmax", name="Classifier (Softmax)")(o)
+
+    ### SUPER-NET CLASSIFIER EXTENSION
+    """
+    extended_net = Sequential(name="ReLu Network")
+    extended_net.add(Dense(20, input_dim=16800, activation="relu"))
+    extended_net.add(Dense(20, activation="relu"))
+    extended_net.add(Dense(20, activation="relu"))
+    soft = Dense(2, activation="softmax", name="Classifier (Softmax)")
+
+    o = soft(extended_net(o))"""
+
+    model = Model(input=inputs, output=o)
 
     ##
     # Generate the optimization method
@@ -138,6 +287,7 @@ def run(model, exp, terms, save_freq=5, data=None):
     num_batches = int(ceil(train_length / exp.batch_size))
 
     valid = Validator(exp, terms)
+    perms = list(gen_permutations(2, 7, 2))
 
     eTimes = np.array([])
     valid._clock = clock()
@@ -150,21 +300,37 @@ def run(model, exp, terms, save_freq=5, data=None):
             print("Epoch {}".format(valid.epochs+1))
         bETA = 0
         bTimes = np.array([])
+        #print("\t Training: ")
         for b in xrange(num_batches):
             bt = clock()
             # Update progress bar
             progress(b, num_batches, exp.batch_size, bETA)
             # Train on a batch
-            model.train_on_batch(x_train[b*exp.batch_size:b*exp.batch_size+exp.batch_size, :],
-                                 y_train[b*exp.batch_size:b*exp.batch_size+exp.batch_size, :])
+            x_batch = x_train[b*exp.batch_size:b*exp.batch_size+exp.batch_size, :]
+            # Pre-train permutations and sorting
+            sort_batch_x = np.zeros((x_batch.shape[0]*len(perms), x_batch.shape[1]))
+            sort_batch_y = np.zeros((x_batch.shape[0], len(perms)))
+            sort_batch_y[:, 0] = [1]*x_batch.shape[0]
+            for i, b in enumerate(x_batch):
+                event = np.zeros((len(perms), x_batch.shape[1]))
+                for j, p in enumerate(perms):
+                    event[j] = np.dot(b, E(p))
+                arange = np.arange(len(perms))
+                np.random.shuffle(arange)
+                sort_batch_x[i*len(perms):(i+1)*len(perms)] = event[arange]
+                sort_batch_y[i] = sort_batch_y[i][arange]
+
+            model.train_on_batch(sort_batch_x, sort_batch_y)
             bTimes = np.append(bTimes, clock()-bt)
             bETA = np.median(bTimes)*(num_batches-b-1)
         # Finish progress bar
         progress(num_batches, num_batches, exp.batch_size, 0, end='\n')
         # Calculate stats and add the epoch results to the experiment object
         epoch = exp.results.add()
-        epoch.train_loss, epoch.train_accuracy = model.evaluate(x_train, y_train, batch_size=exp.batch_size, verbose=2)
-        epoch.test_loss, epoch.test_accuracy = model.evaluate(x_test, y_test, batch_size=exp.batch_size, verbose=2)
+        #print("\t Evaluating training:\n\t ", end='')
+        epoch.train_loss, epoch.train_accuracy = model.evaluate(x_train, y_train, batch_size=exp.batch_size, verbose=0)
+        #print("\t Evaluating testing:\n\t ", end='')
+        epoch.test_loss, epoch.test_accuracy = model.evaluate(x_test, y_test, batch_size=exp.batch_size, verbose=0)
         epoch.s_b = st.significance(model, data)
         epoch.auc = st.AUC(model, data, experiment_epoch=epoch)
         for r in st.num_of_each_cell(model, data):
@@ -187,7 +353,7 @@ def run(model, exp, terms, save_freq=5, data=None):
 
         if (len(exp.results) % save_freq) == 0:
             save(model, exp, save_dir, exp_file_name)
-            print("\t Saved the model\n")
+            print("\t ", end='')
         sys.stdout.flush()
 
     exp.end_date_time = str(datetime.datetime.now())
@@ -197,6 +363,7 @@ def run(model, exp, terms, save_freq=5, data=None):
     print("Total Time: {}".format(convert_seconds(valid.time)))
 
     save(model, exp, save_dir, exp_file_name, graph=True)
+    print("\t ", end='')
 
 def save(model, exp, save_dir, exp_file_name, graph=False):
     ##
@@ -207,15 +374,29 @@ def save(model, exp, save_dir, exp_file_name, graph=False):
         os.makedirs(save_dir)
     os.chdir(save_dir)
 
-    with open(exp_file_name, "wb") as experiment_file:
-        experiment_file.write(exp.SerializeToString())
+    try:
+        with open(exp_file_name, "wb") as experiment_file:
+            experiment_file.write(exp.SerializeToString())
+    except Exception as e:
+        print("Failed to save experiment object: {}".format(e))
 
-    config = model.to_json()
-    with open("cfg.json", 'w') as fp:
-        fp.write(config)
+    config = model.get_config()
+    try:
+        with open("cfg.json", 'w') as fp:
+            fp.write(model.to_json())
+    except Exception as e:
+        print("Failed to save json configuration: {}\n{}".format(e, config))
 
-    w = model.get_weights()
-    np.save("weights", w)
+    try:
+        w = model.get_weights()
+        np.save("weights", w)
+    except Exception as e:
+        print("Failed to save model weights (numpy): {}".format(e))
+
+    """try:
+        model.save_weights("weights.h5", overwrite=True)
+    except Exception as e:
+        print("Failed to save model weights (hdf5): {}".format(e))"""
 
     if graph:
         gr.s_b(exp)
@@ -225,6 +406,8 @@ def save(model, exp, save_dir, exp_file_name, graph=False):
         plt.tight_layout()
         plt.savefig("{}{}{}.png".format(save_dir, os.sep, exp.description), format="png")
         plt.clf()
+
+    print("Saved the model data{}".format(" and graph" if graph else '') + " to {}\n".format(save_dir))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -290,6 +473,9 @@ if __name__ == "__main__":
 
 
 """
+
+Look at new data on full and also on data amount equivalent to old
+
 After training finishes, this is the dump structure:
 
 --> <Custom-name>
